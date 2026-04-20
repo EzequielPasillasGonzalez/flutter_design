1203390015e41d817da582fde0a0127bce5a8f64
Ezequiel <ingepasillasg@gmail.com>
2026-04-20 10:08:05 -0600

    feat: creacion de nuevos componentes

---
+++ lib/src/widgets/headers_widgets.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 300, color: Color(0xff615AAB));
+  }
+}
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  const HeaderBordesRedondeados({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(70),
+          bottomRight: Radius.circular(70),
+        ),
+        color: Color(0xff615AAB),
+      ),
+    );
+  }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 300, color: Color(0xff615AAB));
+  }
+}
